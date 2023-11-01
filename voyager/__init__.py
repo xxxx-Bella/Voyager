8e14f76e33f0f13f8d898cc8c6e5c25f797a91db
xxxx-Bella <hyx18390659623@163.com>
2023-11-01 15:19:23 +0800

    init

--- voyager/__init__.py
+++ voyager/__init__.py
@@ -2,7 +2,7 @@
 from voyager import Voyager
 
 
-# You can also use mc_port instead of azure_login, but azure_login is highly recommended
+# You can also use mc_port instead of azure_login, but azure_login is highly recommended.
 azure_login = {
     "client_id": "1f10bb64-dbd3-4743-917b-4d7c552ce66e",
     "redirect_url": "https://127.0.0.1/auth-response",
